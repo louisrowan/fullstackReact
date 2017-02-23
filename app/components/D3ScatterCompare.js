2e25b0e547771c79175818e882b6666fca317b6d
Louis Rowan <louis.rowan@icloud.com>
2017-02-22 23:07:40 -0800

    beings transition of moving scatter plot to react component

---
+++ app/components/D3ScatterCompare.js
@@ -0,0 +1,17 @@
+const React = require('react')
+
+const D3ScatterCompare = React.createClass({
+  componentDidMount(){
+    this.renderChart()
+  },
+  renderChart(){
+
+  },
+  render(){
+    return (
+
+    )
+  }
+})
+
+module.exports = D3ScatterCompare
