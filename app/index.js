27f2b4c0db16fbe8d59ee54cd70780a5d39816cf
Louis Rowan <louis.rowan@icloud.com>
2017-02-27 17:56:46 -0800

    deletes old side project work for todolist

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const { Router, hashHistory, Route} = require('react-router')
-const Show = require('./components/Show')
-const App = require('./components/App')
-const BaseballContainer = require('./containers/BaseballContainer')
 const ScatterContainer = require('./containers/ScatterContainer')
 require('../public/styles.css')
 require('../public/scatter.css')
@@ -12,11 +9,7 @@ const Routes = React.createClass({
   render() {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={App} />
-        <Route path='/parse' component={BaseballContainer} />
-        <Route path='/d3Scatter' component={ScatterContainer} />
-        <Route path='/:id' component={Show} />
-        
+        <Route path='/' component={ScatterContainer} />
       </Router>
     )
   }
