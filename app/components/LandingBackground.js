b962487e88e74d786f499946c33e428a39bddfb1
Louis Rowan <louis.rowan@icloud.com>
2017-02-28 17:54:13 -0800

    finishes background swap feature

--- app/components/LandingBackground.js
+++ app/components/LandingBackground.js
@@ -1,11 +1,39 @@
 const React = require('react')
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group')
 
 const LandingBackground = React.createClass({
+  getInitialState(){
+    return {
+      index: 0,
+      urls: [
+    'https://media-cdn.tripadvisor.com/media/photo-s/07/56/5f/16/love-ya-fenway-park.jpg',
+    'http://www.ballparksofbaseball.com/wp-content/uploads/2016/03/yank10954.jpg',
+    'http://thedailydeelight.com/wp-content/uploads/2016/06/camden-yards.jpg'
+      ]
+    }
+  },
+  componentDidMount(){
+    setInterval(function(){
+      var next;
+      if (this.state.index === this.state.urls.length - 1) {
+        next = 0
+      } else {
+        next = this.state.index + 1
+      }
+      this.setState({ index: next})
+    }.bind(this), 4000)
+  },
   render(){
-    var url = 'https://media-cdn.tripadvisor.com/media/photo-s/07/56/5f/16/love-ya-fenway-park.jpg'
+    var { urls, index } = this.state
+
+    var content = urls.map((url, i) => {
+      return <img key={i} src={url} />
+    })
     return (
         <div id='backgroundImage'>
-          <img src={url} />
+
+            {content[index]}
+
           <div id='backgroundTint'></div>
         </div>
     )
